bafc1d25cee2ba15641e5da16f895bd1a6c74f63
Katie Ha <katie.ha98@gmail.com>
2025-05-12 20:43:00 -0400

    Update and Delete business backend route

--- backend/app/api/business_routes.py
+++ backend/app/api/business_routes.py
@@ -51,3 +51,53 @@ def create_business():
     db.session.add(business)
     db.session.commit()
     return jsonify(business.to_dict())
+
+
+@business_routes.route("/", methods=["PUT"])
+@login_required
+def update_business():
+    """
+    Update a business
+    """
+    business = Business.query.get(id)
+    
+    if not business:
+        return {"errors": {"message": "Business not found"}}, 404
+    
+    if business.owner_id != current_user.id:
+        return {"errors": {"message": "Unauthorized"}}, 401
+    
+    data = request.get_json()
+    business.name = data.get("name", business.name)
+    business.country = data.get("country", business.country)
+    business.address = data.get("address", business.address)
+    business.city = data.get("city", business.city)
+    business.state = data.get("state", business.state)
+    business.zipcode = data.get("zipcode", business.zipcode)
+    business.type = data.get("type", business.type)
+    business.description=data.get("description",business.description)
+    business.price_range = data.get("price_range", business.price_range)
+    business.lat=data.get("lat",business.lat)
+    business.lng=data.get("lng",business.lng)
+
+    db.session.commit()
+    return jsonify(business.todict())
+
+
+@business_routes.route("/<id>", methods=["DELETE"])
+@login_required
+def delete_business(id):
+    """
+    Delete a business
+    """
+    business = Business.query.get(id)
+
+    if not business:
+        return {"errors": {"message": "Business not found"}}, 404
+    
+    if business.owner_id != current_user.id:
+        return {"errors": {"message": "Unauthorized"}}, 401
+    
+    db.session.delete(business)
+    db.session.commit()
+    return {"message": "Successfully deleted"}
--- backend/app/seeds/__init__.py
+++ backend/app/seeds/__init__.py
@@ -1,5 +1,6 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .businesses import seed_businesses, undo_businesses
 
 from app.models.db import db, environment, SCHEMA
 
@@ -19,6 +20,7 @@ def seed():
         undo_users()
     seed_users()
     # Add other seed functions here
+    seed_businesses()
 
 
 # Creates the `flask seed undo` command
