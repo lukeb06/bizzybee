7e0a8f9bf5daf5ef1a9f7752497073653be35740
FaboDaGreat <fabian.richmond92@gmail.com>
2025-05-13 21:23:18 -0400

    Created route for deleting a review

--- backend/app/api/review_routes.py
+++ backend/app/api/review_routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify
 from app.models import Review
+from flask_login import login_required, current_user
+from app.models.db import db
 
 review_routes = Blueprint("review", __name__)
 
@@ -10,3 +12,20 @@ def reviews(business_id: int ):
 
     reviews = Review.query.filter(Review.businessId == business_id).all()
     return jsonify([r.to_dict() for r in reviews])
+
+
+@review_routes.route("/business/<business_id>/reviews/<review_id>", methods=["DELETE"])
+@login_required
+def delete_review(review_id):
+    review = Review.query.get(review_id)
+    
+    if not review:
+        return jsonify({"error": "Review not found"}), 404
+
+    if review.userId != current_user.id:
+        return jsonify({"error": "Cannot delete e review you did not leave"}), 401
+
+    db.session.delete(review)
+    db.session.commit()
+
+    return jsonify({"message": "Successfully deleted"})
