ae94528156babd9da32b8b234c24a3cd568442a2
FaboDaGreat <fabian.richmond92@gmail.com>
2025-05-15 19:53:17 -0400

    Added and completed route for editing a review in the backend

--- backend/app/api/review_routes.py
+++ backend/app/api/review_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from app.models import Review
 from flask_login import login_required, current_user
 from app.models.db import db
@@ -14,7 +14,7 @@ def reviews(business_id: int ):
     return jsonify([r.to_dict() for r in reviews])
 
 
-@review_routes.route("/business/<business_id>/reviews/<review_id>", methods=["DELETE"])
+@review_routes.route("<review_id>", methods=["DELETE"])
 @login_required
 def delete_review(review_id):
     review = Review.query.get(review_id)
@@ -23,9 +23,38 @@ def delete_review(review_id):
         return jsonify({"error": "Review not found"}), 404
 
     if review.userId != current_user.id:
-        return jsonify({"error": "Cannot delete e review you did not leave"}), 401
+        return jsonify({"error": "Cannot delete a review you did not leave"}), 401
 
     db.session.delete(review)
     db.session.commit()
 
     return jsonify({"message": "Successfully deleted"})
+
+@review_routes.route("<review_id>", methods=["PUT"])
+@login_required
+def edit_review(review_id):
+    review = Review.query.get(review_id)
+
+    if not review:
+        return jsonify({"error": "Review not found"}), 404
+
+    if review.userId != current_user.id:
+        return jsonify({"error": "Cannot edit a review you did not leave"}), 401
+    
+
+    data = request.get_json()
+
+    if (not isinstance(data["review"], str) or data["review"] == "" or len(data["review"]) > 1000): 
+        return jsonify({"error": "Review must be at least 1 character and less than 1000 characters"}), 400
+        
+    if (not isinstance(data["stars"], int) or data["stars"] < 1 or data["stars"] > 5):
+        return jsonify({"error": "Star ratings must be a number from 1 to 5"}), 400
+
+
+    
+    review.review = data.get("review", review.review)
+    review.stars = data.get("stars", review.stars)
+
+    db.session.commit()
+    return jsonify(review.todict())
+
