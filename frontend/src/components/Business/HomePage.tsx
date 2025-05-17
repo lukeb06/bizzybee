f187f9e159f11b4894b59bbc4f1720e015430ddc
Katie Ha <katie.ha98@gmail.com>
2025-05-16 22:16:44 -0400

    Create home page

---
+++ frontend/src/components/Business/HomePage.tsx
@@ -0,0 +1,35 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { thunkGetAllBusinesses } from '../../redux/business';
+// import './HomePage.css';
+import { useNavigate } from 'react-router-dom';
+import { RootState } from '../../redux/store';
+import { useEffect } from 'react';
+
+// interface ISignUpErrors {
+//     server?: any;
+//     email?: string;
+//     username?: string;
+//     password?: string;
+//     confirmPassword?: string;
+// }
+
+export default function HomePage() {
+    const allBusinesses = useSelector((state: RootState) => state.businesses.allBusinesses);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    console.log('Businesses:', allBusinesses);
+
+    useEffect(() => {
+        const getBusinesses = async () => {
+            dispatch(thunkGetAllBusinesses());
+            getBusinesses();
+        };
+    }, [dispatch, allBusinesses]);
+
+    return (
+        <>
+            <h1> Welcome to Home Page</h1>
+        </>
+    );
+}
