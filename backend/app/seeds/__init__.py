c674b6376f1a6f75a43e6b145d35f84d37d7235c
mace-stev <williamstevenson107@gmail.com>
2025-05-16 20:19:41 -0500

    fixed seeder file

--- backend/app/seeds/__init__.py
+++ backend/app/seeds/__init__.py
@@ -1,8 +1,10 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .businesses import seed_businesses, undo_businesses
+from .reviews import seed_reviews, undo_reviews
+from .images import seed_images, undo_images
 
-from app.models.db import db, environment, SCHEMA
+from app.models import db, environment, SCHEMA
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -21,10 +23,15 @@ def seed():
     seed_users()
     # Add other seed functions here
     seed_businesses()
+    seed_reviews()
+    seed_images()
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command("undo")
 def undo():
     undo_users()
+    undo_businesses()
+    undo_reviews()
+    undo_images()
     # Add other undo functions here
