0ad62e339ed8e31a0e73b42c6828e89edfeb2350
tallha07 <tallhamuddassar@yahoo.com>
2025-05-23 18:30:08 -0500

    something minor

--- backend/app/api/image_routes.py
+++ backend/app/api/image_routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, request, jsonify
 from app.models import Image, db
 from flask_login import login_required, current_user
+
+
 image_routes = Blueprint("image", __name__, url_prefix="/images")
 
 
