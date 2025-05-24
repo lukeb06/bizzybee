aeec8d4711c08d5ee846b50ecf63b839fc8162ad
Katie Ha <katie.ha98@gmail.com>
2025-05-23 20:10:40 -0400

    Updated the functionality of view business page

--- frontend/src/components/Business/BusinessPage.tsx
+++ frontend/src/components/Business/BusinessPage.tsx
@@ -3,100 +3,104 @@ import ReviewStar from '../ReviewStar/ReviewStar';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useAppSelector } from '../../redux/store';
 import Reviews from '../Reviews/Reviews';
+import './BusinessPage.css';
 
 const BusinessDetailPage: React.FC = () => {
-
     const navigate = useNavigate();
-    const { businessId } = useParams();
+    const { businessId } = useParams<{ businessId: string }>();
+    const business = useAppSelector(state =>
+        businessId ? state.businesses.byId[Number(businessId)] : undefined,
+    );
 
     const [isLoaded, setIsLoaded] = useState(false);
-    const business = useAppSelector((state) => state.businesses.byId[Number(businessId)])
 
-
-
-    const fakeBusiness = {
-        title: "Tabla Indian Cafe & Sweets Orlando",
-        reviews: "5.0",
-        category: "Indian",
-        images: ['https://rasushi.com/wp-content/uploads/2023/03/DSC08947_SUSHI-PLATTER.jpg']
-    }
+    useEffect(() => {
+        if (!businessId || isNaN(Number(businessId)) || !business) {
+            navigate('/');
+            return;
+        }
+        setIsLoaded(true);
+    }, [businessId, business, isLoaded]);
 
     const handleDeleteBusiness = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault();
         // TODO:
-
-    }
+    };
     const handleUpdateBusiness = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault();
         // TODO navigate to the existing create form page, where everything should be prefilled:
-
-    }
+    };
 
     let reviewCount = 0;
 
     const checkReviewCount = (reviews: string): string | void => {
         const reviewNum = parseInt(reviews);
         if (reviewNum > 5 || reviewNum < 1) {
-            return "error. todo later"
+            return 'error. todo later';
         } else {
             reviewCount = reviewNum;
         }
+    };
+
+    const fakeReviews = ['Hello', 'Bye', 'Food tasted great'];
+
+    if (!isLoaded || !business) {
+        return <div>Spot not found</div>;
     }
 
-    console.log(business)
-    const fakeReviews = [
-        "Hello",
-        "Bye",
-        "Food tasted great"
-    ]
+    const featuredImage = business.featured_image;
 
-
-    useEffect(() => {
-        console.log(typeof businessId, "what is this?")
-        if ((businessId === '0')) {
-            console.log(businessId, "here")
-            navigate('/')
-            setIsLoaded(false);
-        } else {
-            setIsLoaded(true);
-        }
-    }, [isLoaded])
-
-    if (!isLoaded) {
-        return null
-    } else {
-
-        return (
-            <div>
-                <h1>{business.name}</h1>
-                {/* Dev items, please delete if not needed, or adjust */}
-                <button onClick={(e) => handleDeleteBusiness(e)}>Delete a businesss</button>
-                <button onClick={(e) => handleUpdateBusiness(e)}>Update a business</button>
-                {/* --------- */}
-                {/* {fakeBusiness.images.map((url, key) => (
-                <div key={`${key}-${url}`}>
-                <img src={url} style={{height: '200px', width: '200px'}} />
-                </div>
-                ))} */}
-                <div>
-                    <img src={business.featured_image} style={{ height: '200px', width: '200px' }} />
-                </div>
-                <div>
-                    <img src={business.preview_image} style={{ height: '200px', width: '200px' }} />
-                </div>
-                {typeof checkReviewCount(business.review_count) === 'string' ?
-                    <div>
-                        <span>errors</span>
-                    </div>
-                    :
-                    <ReviewStar reviewCount={business.average_rating} />
-                }
-
-                <Reviews reviews={fakeReviews} />
-
+    return (
+        <div className="business-page">
+            <div className="image-gallery">
+                <img className="main-image" src={featuredImage} alt="" />
             </div>
-        );
-    }
+
+            <div className="detail-container">
+                <div className="suggested-buttons">
+                    <button>Write a Review</button>
+                    <button>Add photos/videos</button>
+                    <button>Share</button>
+                    <button>Save</button>
+                </div>
+                <div className="business-info">
+                    <h1>{business.name}</h1>
+                    <div className="ratings">
+                        <span>{'*'.repeat(Math.floor(business.average_rating))}</span>
+                        <span>
+                            {business.average_rating} {business.review_count} reviews
+                        </span>
+                    </div>
+                    <div className="other-info">
+                        <span>{business.price_range}</span>
+                        <span>{business.category}</span>
+                    </div>
+                    <div className="spot-description">
+                        <p>{business.description}</p>
+                    </div>
+                </div>
+                <div className="callout-box">
+                    <h2>Order Food</h2>
+                    <div className="delivery-detail">
+                        <span>Free Delievery</span>
+                        <span>$0 min</span>
+                        <span>30-40 min</span>
+                    </div>
+                    <input type="text" placeholder="Delivery Address" />
+                    <button>Start Order</button>
+                    <p>Get directions</p>
+                    <p>{business.address}</p>
+                </div>
+            </div>
+            {/* Dev items, please delete if not needed, or adjust */}
+            <button onClick={e => handleDeleteBusiness(e)}>Delete a businesss</button>
+            <button onClick={e => handleUpdateBusiness(e)}>Update a business</button>
+
+            <ReviewStar reviewCount={business.average_rating} />
+
+            <Reviews reviews={fakeReviews} />
+        </div>
+    );
 };
 
 export default BusinessDetailPage;
